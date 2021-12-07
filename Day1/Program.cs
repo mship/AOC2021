8f31894193283898b22a5f9b86e8fd199ae24722
mship <meshippee@gmail.com>
2021-12-06 17:29:19 -0700

    Add files via upload

---
+++ Day1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Day1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"input.txt");
+            int lastNum = 0;
+            int retVal = -1;
+            foreach(string line in lines){
+                int thisNum = Int32.Parse(line);
+                if(thisNum > lastNum){
+                    retVal++;
+                }
+                lastNum = thisNum;
+
+            }
+            Console.WriteLine(retVal);
+        }
+    }
+}
